0b4f4e236886e17484b01c433f30daff01d7d66b
Praveenps18 <praveenps832002@gmail.com>
2024-01-02 10:59:53 +0530

    message

---
+++ javascript/arthimetic.js
@@ -0,0 +1,27 @@
+let a=10
+let b=5
+let c=a+b
+let d=a-b
+console.log(c)
+console.log(d)
+let A=20
+let B=15
+let C=A%B
+console.log(C)
+let ab=100
+let bc="100"
+console.log(ab==bc)
+let AB=5
+AB+=10
+console.log(AB)
+let e=5
+let f=10
+let g=10
+console.log(e!=g)
+let h=true
+let i=false
+console.log(h&&i)
+let H=false
+let I=true
+console.log(H||I)
+console.log(!H)
